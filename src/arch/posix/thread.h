d3d8d7d2d0b3a3b56c080cb1563612ce53fb164b
Andrea della Porta <sfaragnaus@gmail.com>
2018-03-15 18:58:39 +0100

    added thread-safety

--- src/arch/posix/thread.h
+++ src/arch/posix/thread.h
@@ -2,14 +2,16 @@
 #define THREAD_GUARD
 
 #include <pthread.h> 
+#include <pthread.h> 
+#include "../../executor.h"
 
 namespace nis {
 	namespace helper {
 		typedef pthread_t NISThread; 
 		typedef void*(*NISThFunc)(void*);
 
-		int NIS_CreateThread(NISThread* th, NISThFunc fnc, void *data);
-		int NIS_JoinThread(NISThread* th);
+		int NIS_CreateThread(NISThread &th, NISThFunc fnc, std::shared_ptr<PollExecutor>);
+		int NIS_JoinThread(NISThread &th);
 	}
 }
 
