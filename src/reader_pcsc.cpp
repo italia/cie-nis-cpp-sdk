374e1bd1dd7b879b12736489e27e09e3d7432be7
Daniele Cocca <dcocca@google.com>
2018-06-16 15:05:24 +0100

    Fix includes on reader_pcsc.*, apply clang-format

--- src/reader_pcsc.cpp
+++ src/reader_pcsc.cpp
@@ -1,51 +1,39 @@
-#include <iostream>
-#include <sstream>
-#include <vector>
-#include <ios>
-#include "token_pcsc.h"
 #include "reader_pcsc.h"
 #include <cstring>
-
-using namespace cie::nis;
+#include "token_pcsc.h"
 
 ReaderPCSC::ReaderPCSC()
-  : hasContext{SCardEstablishContext(
-        SCARD_SCOPE_SYSTEM, nullptr, nullptr, &context) == SCARD_S_SUCCESS}
-{
+    : hasContext{SCardEstablishContext(SCARD_SCOPE_SYSTEM, nullptr, nullptr,
+                                       &context) == SCARD_S_SUCCESS} {}
+
+ReaderPCSC::~ReaderPCSC() {
+  if (hasContext) SCardReleaseContext(context);
 }
 
-ReaderPCSC::~ReaderPCSC()
-{
-	if(hasContext)
-		SCardReleaseContext(context);
-}
+std::vector<std::string> ReaderPCSC::getReaderList() { return readerList; }
 
-vector<string> ReaderPCSC::getReaderList()
-{
-	return readerList;
-}
+ReaderResult ReaderPCSC::enumerateReaderList() {
+  readerList.clear();
+  tokenList.clear();
 
-ReaderResult ReaderPCSC::enumerateReaderList()
-{
-	readerList.clear();
-	tokenList.clear();
+  DWORD dwReaders;
+  if (SCardListReaders(context, nullptr, nullptr, &dwReaders) !=
+      SCARD_S_SUCCESS)
+    return READER_RESULT_GENERIC_ERROR;
 
-	DWORD dwReaders;
-	if (SCardListReaders(context, nullptr, nullptr, &dwReaders) != SCARD_S_SUCCESS)
-		return READER_RESULT_GENERIC_ERROR;
+  char mszReaders[dwReaders] = {'\0'};
+  if (SCardListReaders(context, nullptr, mszReaders, &dwReaders) !=
+      SCARD_S_SUCCESS)
+    return READER_RESULT_GENERIC_ERROR;
 
-	char mszReaders[dwReaders] = {'\0'};
-	if (SCardListReaders(context, nullptr, mszReaders, &dwReaders) != SCARD_S_SUCCESS)
-		return READER_RESULT_GENERIC_ERROR;
+  char* reader = mszReaders;
+  while (*reader) {
+    std::string name{reader};
+    reader += strlen(reader) + 1;
 
-	char* reader = mszReaders;
-	while (*reader) {
-		string name{reader};
-		reader += strlen(reader) + 1;
+    readerList.emplace_back(name);
+    tokenList.emplace(name, std::make_shared<TokenPCSC>(name, context));
+  }
 
-		readerList.emplace_back(name);
-		tokenList.emplace(name, std::make_shared<TokenPCSC>(name, context));
-	}
-
-	return READER_RESULT_OK;
+  return READER_RESULT_OK;
 }
