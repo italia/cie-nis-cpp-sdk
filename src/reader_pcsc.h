cfae475fa53cd7d32478d7b87544402fc7bedc5d
Daniele Cocca <daniele.cocca@gmail.com>
2018-06-16 15:05:24 +0100

    Fix includes on reader_pcsc.*, apply clang-format

--- src/reader_pcsc.h
+++ src/reader_pcsc.h
@@ -1,7 +1,8 @@
-/** @filereader_pcsc.h 
+/** @filereader_pcsc.h
  *  @brief The PCSC-specific implementation of a Reader
  *
- *  Contains the PCSC-centric implementation of a reader, managing all PCSC compatible reader. Based on libpcsc-lite.
+ *  Contains the PCSC-centric implementation of a reader, managing all PCSC
+ * compatible reader. Based on libpcsc-lite.
  *  @see Reader
  *  */
 
@@ -9,32 +10,32 @@
 #define READER_PCSC_GUARD
 
 #include <winscard.h>
+#include <string>
+#include <vector>
 #include "reader.h"
 
-using namespace cie::nis;
-using namespace std;
-
 namespace cie {
-	namespace nis {
-		class ReaderPCSC : public Reader
-		{
-		private:
-			/** hold the pscs-lite context */
-			SCARDCONTEXT context;
+namespace nis {
 
-			/** multi-string containing the connected Reader's names */
-			vector<string> readerList;
+class ReaderPCSC : public Reader {
+ private:
+  // hold the pscs-lite context
+  SCARDCONTEXT context;
 
-		public:
-			/** flag to indicate whether the pcsc context has been obtained (succesfully) */
-			const bool hasContext = false;
+  // multi-string containing the connected Reader's names
+  std::vector<std::string> readerList;
 
-			ReaderPCSC();
-			~ReaderPCSC();
-			ReaderResult enumerateReaderList();
-			vector<string> getReaderList();
-		};
-	}
-}
+ public:
+  // flag to indicate whether the pcsc context has been obtained successfully
+  const bool hasContext = false;
 
-#endif
+  ReaderPCSC();
+  ~ReaderPCSC();
+  ReaderResult enumerateReaderList();
+  std::vector<std::string> getReaderList();
+};
+
+}  // namespace nis
+}  // namespace cie
+
+#endif  // READER_PCSC_GUARD
