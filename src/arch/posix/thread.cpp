56e0b6f613d15e5b36ad660635be2aac5320c7c2
Daniele Cocca <daniele.cocca@gmail.com>
2018-06-16 10:53:59 +0100

    Fix includes on thread.*, apply clang-format

--- src/arch/posix/thread.cpp
+++ src/arch/posix/thread.cpp
@@ -1,35 +1,33 @@
-#include <iostream>
 #include "thread.h"
-#include "../../executor.h"
+#include <pthread.h>
+#include <iostream>
 
 namespace cie {
-    namespace nis {
-	namespace helper
-	{
-		int NIS_CreateThread(NISThread &th, NISThFunc fnc, std::weak_ptr<PollExecutor> pe)
-		{
-			if(pe.expired())
-				return -2;
+namespace nis {
+namespace helper {
 
-			std::shared_ptr<PollExecutor> cntr{pe};
-			if(pthread_create(&th, NULL, fnc, pe.lock().get())) {
-				std::cerr << "Error creating thread" << std::endl;
-				return -1;
-			}
+int NIS_CreateThread(NISThread &th, NISThFunc fnc,
+                     std::weak_ptr<PollExecutor> pe) {
+  if (pe.expired()) return -2;
 
-			return 0;
-		}
+  std::shared_ptr<PollExecutor> cntr{pe};
+  if (pthread_create(&th, NULL, fnc, pe.lock().get())) {
+    std::cerr << "Error creating thread" << std::endl;
+    return -1;
+  }
 
-		int NIS_JoinThread(NISThread &th)
-		{
-			if(pthread_join(th, NULL)) {
-				std::cerr << "Error joining thread" << std::endl;
-				return -1;
-			}
-
-			return 0;
-		}
-	}
-    }
+  return 0;
 }
 
+int NIS_JoinThread(NISThread &th) {
+  if (pthread_join(th, NULL)) {
+    std::cerr << "Error joining thread" << std::endl;
+    return -1;
+  }
+
+  return 0;
+}
+
+}  // namespace helper
+}  // namespace nis
+}  // namespace cie
--- src/arch/posix/thread.h
+++ src/arch/posix/thread.h
@@ -1,20 +1,22 @@
 #ifndef THREAD_GUARD
 #define THREAD_GUARD
 
-#include <pthread.h> 
+#include <memory>
 #include <pthread.h> 
 #include "../../executor.h"
 
 namespace cie {
-    namespace nis {
-	namespace helper {
-		typedef pthread_t NISThread; 
-		typedef void*(*NISThFunc)(void*);
+namespace nis {
+namespace helper {
 
-		int NIS_CreateThread(NISThread &th, NISThFunc fnc, std::weak_ptr<PollExecutor>);
-		int NIS_JoinThread(NISThread &th);
-	}
-    }
-}
+typedef pthread_t NISThread;
+typedef void *(*NISThFunc)(void *);
 
-#endif
+int NIS_CreateThread(NISThread &th, NISThFunc fnc, std::weak_ptr<PollExecutor>);
+int NIS_JoinThread(NISThread &th);
+
+}  // namespace helper
+}  // namespace nid
+}  // namespace cie
+
+#endif  // THREAD_GUARD
